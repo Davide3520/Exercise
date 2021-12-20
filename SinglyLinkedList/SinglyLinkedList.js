3c8c41c00985595d7bdc7b3e88edba4435def67a
Davide Lepri <davidelepri3@gmail.com>
2021-12-20 12:58:04 -0500

    pop added

--- SinglyLinkedList/SinglyLinkedList.js
+++ SinglyLinkedList/SinglyLinkedList.js
@@ -26,6 +26,32 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
+list.push(1)
+list.push(2)
+list.push(3);
+
