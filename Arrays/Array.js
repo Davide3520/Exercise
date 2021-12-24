5aaa43d51e5e23675236bff420d356aac42fa1d4
Davide Lepri <davidelepri3@gmail.com>
2021-12-24 11:43:12 -0500

    Step 1

--- Arrays/Array.js
+++ Arrays/Array.js
@@ -1,11 +1,14 @@
 /*
-
+PROMPT:
 Given an array of integers, return the indices of the two numbers that add
 up to a given target.
 
-Step 1:  ASK ABOUT EDGE CASE;
 
+Step 1:  ASK ABOUT EDGE CASE WHEN DEALING WITH INTEGERS
 
-
-
+- Are all numbers positive or there can be negative numbers?
+- Are there duplicate numbers in the array?
+- Will there always be a solution available?
+- What do we return if there is no solution available?
+- Write out some test case.
 */
