5f809b719b39e7af48c5bf7ce5e69bc29c1d23f1
Davide Lepri <davidelepri3@gmail.com>
2021-12-27 16:11:46 -0500

    step 2 added

--- Arrays/ContainerMostWater.js
+++ Arrays/ContainerMostWater.js
@@ -5,5 +5,15 @@ Return the maximum amount of water a container can store.
 
 STEP 1 EDGE CASES:
 - Does the thickness of the line affects the area?
+- Do the left and right side count as walls?
 - Does the higher line affect our area?
-*/
+
+STEP 2 Test Cases:
+
+We need to calculate the area of the container which is L x W  (Length, Width). The Length would be the height
+of the container, which is the smallest of the 2 sides. The Width is the distance between the two lines in the x axis.
+example: [1, 8, 6, 2, 9, 4] = L-> 8 x (4 - 1) = 24. 4 -> position highest number. 1-> position second largest number(8).
+
+[7, 2, 3, 4, 9] 7 x 4 = 28.
+
+*/      
