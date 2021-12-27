327f7907ada82e1d30cbe6917051facdacc6c6e8
Davide Lepri <davidelepri3@gmail.com>
2021-12-27 15:55:29 -0500

    prompt

--- Arrays/twoSums.js
+++ Arrays/twoSums.js
@@ -22,6 +22,16 @@ STEP 4: WRITE OUT THE SOLUTION IN CODE.
 BRUTE FORCE
 */
 
+/*
+
+twoSum([1,2,3,8,4], 12) [3,4]
+twoSum([1], 1) null
+twoSum([], 5) null
+twoSum([1,2,3], 25) null
+twoSum([1,2], 3) [0, 1]
+
+*/
+
 // const twoSums = (arr, tar) => {
 //   for (let i = 0; i < arr.length; i++) {
 //       let numToFind = tar - arr[i]
@@ -34,22 +44,23 @@ BRUTE FORCE
 //   return null;
 // }
 
-const twoSums = (arr, target) => {
-  let hashMap = {};
-  for (let i = 0; i < arr.length; i++) {
-    let current = hashMap[arr[i]];
-    if (current >= 0) {
-      return [current, i];
-    }else {
-      let ntf = target - arr[i];
-      hashMap[ntf] = i;
-    }
-  }
-  return null;
-}
+// const twoSums = (arr, target) => {
+//   let hashMap = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     let current = hashMap[arr[i]];
+//     if (current >= 0) {
+//       return [current, i];
+//     }else {
+//       let ntf = target - arr[i];
+//       hashMap[ntf] = i;
+//     }
+//   }
+//   return null;
+// }
 
-console.log(twoSums([1,5,3,12, 9, 2], 11)) // [4, 5];
-console.log(twoSums([1,2,3,4], 25)) // null;
-console.log(twoSums([1], 1)) // null;
-console.log(twoSums([], 5)) // null;
-console.log(twoSums([1,2], 3)) // [0, 1];
+// console.log(twoSums([1,5,3,12, 9, 2], 11)) // [4, 5];
+// console.log(twoSums([1,2,3,4], 25)) // null;
+// console.log(twoSums([1], 1)) // null;
+// console.log(twoSums([], 5)) // null;
+// console.log(twoSums([1,2], 3)) // [0, 1];
+
