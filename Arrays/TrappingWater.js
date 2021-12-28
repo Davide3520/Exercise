752ed3d96819afef081214164e69a61b025478b7
Davide Lepri <davidelepri3@gmail.com>
2021-12-28 17:21:46 -0500

    optimal solution

--- Arrays/TrappingWater.js
+++ Arrays/TrappingWater.js
@@ -47,16 +47,29 @@ step 1:
 4- repeat for other pointer
 */
 
-const trappedWater = (height) => {
-  let totalWater = 0;
-  let maxLeft = 0;
-  let maxRight = height.length - 1;
+const getTrappedRainwater = function(heights) {
 
-  while(maxLeft < maxRight) {
-    let min = Math.min(height[maxLeft], height[maxRight]);
-    if (maxLeft > height[maxLeft]) {
+  let left = 0, right = heights.length - 1, totalWater = 0, maxLeft = 0, maxRight = 0;
 
+  while(left < right) {
+    if(heights[left] <= heights[right]) {
+      if(heights[left] >= maxLeft) {
+        maxLeft = heights[left]
+      } else {
+        totalWater += maxLeft - heights[left];
+      }
+      left++;
+    } else {
+      if(heights[right] >= maxRight) {
+          maxRight = heights[right];
+      } else {
+          totalWater += maxRight - heights[right];
+      }
+
+      right--;
     }
   }
+
+  return totalWater;
 }
-console.log(trappedWater([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]))
+console.log(getTrappedRainwater([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]))
