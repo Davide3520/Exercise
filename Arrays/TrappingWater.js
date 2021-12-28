68fa87b4e5bd57439a1689248f925c38224b44c4
Davide Lepri <davidelepri3@gmail.com>
2021-12-28 12:36:14 -0500

    brute force

--- Arrays/TrappingWater.js
+++ Arrays/TrappingWater.js
@@ -1,3 +1,38 @@
 /*
 Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+step 1:
+
+- do the left and right sides of the graph count as walls?
+- will there be negative integers?
 */
+
+// BRUTE FORCE
+
+const trappedWater = (height) => {
+  let totalWater = 0;
+
+  for (let p = 0; p < height.length; p++) {
+    let leftP = p, rightP = p;
+    let maxRight = 0, maxLeft = 0;
+
+    while (leftP >= 0) {
+      maxLeft = Math.max(maxLeft, height[leftP])
+      leftP--;
+    }
+
+    while (rightP < height.length) {
+      maxRight = Math.max(maxRight, height[rightP]);
+      rightP++;
+    }
+
+    let current = Math.min(maxLeft, maxRight) - height[p];
+
+    if (current >= 0) {
+      totalWater += current;
+    }
+  }
+  return totalWater;
+}
+
+console.log(trappedWater([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]))
