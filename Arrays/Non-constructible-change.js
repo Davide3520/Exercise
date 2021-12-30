bd588141b7c2a8952cc117048e0de58aa72eeb5b
Davide Lepri <davidelepri3@gmail.com>
2021-12-30 16:33:47 -0500

    prompt

---
+++ Arrays/Non-constructible-change.js
@@ -0,0 +1,6 @@
+/*
+Given an array of positive integers representing the values of coins in your possessions, write a function that returns the minimum amount of change that you CANNOT create.
+*/
+
+
+console.log(nonConstrChange([5, 7, 1, 1, 2, 3, 22])) // 20
