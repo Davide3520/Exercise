d043b9c1b16e7a546567f79c1c96d9dd6b3f1a02
Davide Lepri <davidelepri3@gmail.com>
2021-12-30 16:44:10 -0500

    prompt added

---
+++ Arrays/3 nums sums.js
@@ -0,0 +1,6 @@
+/*
+Write a function that takes a non empty array of distinct integers and an integer representing a target sum. the function should find all triplets in the array that sum upp to the target sum and return a 2D array of all the triplets.  The numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect the numbers they hold.
+If no number sums up to the targe it should return an empty array.
+*/
+
+console.log(treeSum([12, 3, 1, 2, -6, 5, -8, 6], 0))
