ba1c066838c2acab0d78020388ff441b799a5f78
Davide Lepri <davidelepri3@gmail.com>
2021-12-30 15:43:09 -0500

    prompt added

---
+++ Arrays/Tournament.js
@@ -0,0 +1,4 @@
+/*
+Given an array of pairs representing the teams that have competed against each other and an array containing the result of each competition, write a function that returns the winner of the tournament. the input arrays are named competitions and results. The competitions array has elements in the form [homeTeam, visitors], where each team is a string of at most 30 charachters representing the name of the team. the results array contains information about the winner of each corresponding competition. Where 1 in the result array means that the home team won and 0 means the visitors won.
+*/
+
