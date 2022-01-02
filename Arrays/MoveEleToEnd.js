1a41cebfaddb16d3ae1ba8e3d4b14681d6a9c554
Davide Lepri <davidelepri3@gmail.com>
2022-01-02 17:43:25 -0500

    solution

--- Arrays/MoveEleToEnd.js
+++ Arrays/MoveEleToEnd.js
@@ -1,3 +1,23 @@
 /*
 You are given an array of integer. write a function that moves all instances of that integer in the array to the end fo the array and return the array. the function should perform this in place (Mutate the input array);
 */
+
+const moveToEnd = (arr, num) => {
+  let i = 0
+  let j = arr.length - 1;
+
+  while (i < j) {
+    while (i < j && arr[j] === num) j--;
+    if (arr[i] === num) swap(i, j, arr);
+    i++;
+  }
+  return arr;
+}
+
+const swap = (i, j, arr) => {
+  const temp = arr[j];
+  arr[j] = arr[i];
+  arr[i] = temp;
+}
+
+console.log((moveToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2)))
