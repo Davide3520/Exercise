233b343cfced8cc28b6acf89469b66e804380fa1
Davide Lepri <davidelepri3@gmail.com>
2022-01-02 16:58:26 -0500

    Move to end prompt added

---
+++ Arrays/MoveEleToEnd.js
@@ -0,0 +1,3 @@
+/*
+You are given an array of integer. write a function that moves all instances of that integer in the array to the end fo the array and return the array. the function should perform this in place (Mutate the input array);
+*/
