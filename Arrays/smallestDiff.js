77c01425f3454a3ef579edccaa998874ea3b9bf8
Davide Lepri <davidelepri3@gmail.com>
2022-01-02 14:41:50 -0500

    Prompt smallest diff added

---
+++ Arrays/smallestDiff.js
@@ -0,0 +1,3 @@
+/*
+Write a function that takes in 2 non empty array of integers, find the pair of numbers one from each array, whose absolute difference is closest to zero. Return an array containing these 2 nunmbers, with the number of the first array in the first position.
+*/
