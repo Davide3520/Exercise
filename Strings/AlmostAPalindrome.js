c4463731153402e0208dcfa84a206d656e98b998
Davide Lepri <davidelepri3@gmail.com>
2022-01-07 14:57:02 -0500

    optimal solution

--- Strings/AlmostAPalindrome.js
+++ Strings/AlmostAPalindrome.js
@@ -12,4 +12,34 @@ Input: s = "abc"
 Output: false
 
 */
+const almostAPalindrome = (s) => {
+  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  let left = 0, right = s.length - 1;
 
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return isPal(s, left + 1, right) || isPal(s, left, right - 1);
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+const isPal = (s, left, right) => {
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(almostAPalindrome("race a car")) // TRUE
+console.log(almostAPalindrome("abccdba")) //TRUE
+console.log(almostAPalindrome("abc")) // FALSE
+console.log(almostAPalindrome('')) // TRUE
+console.log(almostAPalindrome('a')) // TRUE
+console.log(almostAPalindrome('ab')) // TRUE
