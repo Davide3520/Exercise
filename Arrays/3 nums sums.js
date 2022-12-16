754ce24d2bc9cb94169d194f1d19f3b4e41adbe8
Davide Lepri <davidelepri3@gmail.com>
2022-12-15 21:35:53 -0500

    testing

--- Arrays/3 nums sums.js
+++ Arrays/3 nums sums.js
@@ -1,5 +1,4 @@
-console.log('ciao')
-
+'testing'
 /*
 Write a function that takes a non empty array of distinct integers and an integer representing a target sum. the function should find all triplets in the array that sum upp to the target sum and return a 2D array of all the triplets.  The numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect the numbers they hold.
 If no number sums up to the targe it should return an empty array.
